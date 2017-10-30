d93f52c75c4d531da5c5edb9630c3d14b102770a
mbisurgi-designfreed <maximiliano@designfreed.com>
2017-10-30 09:32:44 -0300

    Cors

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const parser = require('body-parser');
+const cors = require('cors');
 const config = require('./config/config');
 
 const app = express();
 
 mongoose.connect(config.mongo.url, { useMongoClient: true });
 
+
 require('./config/passport/passport');
 
+app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
 
