5a5b66e017ec77e583344963664280c1f3986793
mbisurgi-designfreed <maximiliano@designfreed.com>
2017-12-06 09:46:17 -0300

    Agregar campos cliente

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const DIAS = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
+const CLASIFICACION = ['a', 'b', 'c'];
+
 const LocationSchema = new Schema({
     type: { type: String, default: 'Point' },
     coordinates: { type: [Number], index: '2dsphere' }
@@ -35,10 +38,44 @@ const TelefonoSchema = new Schema({
     }
 });
 
+const PersonaInteres = new Schema({
+    tipo: {
+        type: String,
+        required: true
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    dni: {
+        type: String
+    },
+    direccion: {
+        type: DireccionSchema
+    },
+    telefono: {
+        type: TelefonoSchema
+    },
+    email: {
+        type: String
+    }
+});
+
 const ClienteSchema = new Schema({
+    codigo: {
+        type: Number,
+        required: true,
+        unique: true
+    },
     razonSocial: {
         type: String,
         required: true
+    },
+    cuit: {
+        type: String
+    },
+    nombreComercial: {
+        type: String,
     },
     direccion: {
         type: DireccionSchema,
@@ -53,10 +90,35 @@ const ClienteSchema = new Schema({
     sucursales: {
         type: [DireccionSchema]
     },
+    clasificacion: {
+        type: String,
+        required: true,
+        default: 'c',
+        enum: CLASIFICACION
+    },
+    diaVisita: {
+        type: String,
+        required: true,
+        enum: DIAS
+    },
+    diaEntrega: {
+        type: String,
+        required: true,
+        enum: DIAS
+    },
+    personasInteres: {
+        type: [PersonaInteres]
+    },
     sincronizado: {
         type: Boolean,
         default: false
     }
+});
+
+ClienteSchema.pre('save', function (next) {
+    const cliente = this;
+
+    
 });
 
 const Cliente = mongoose.model('cliente', ClienteSchema, 'clientes');
