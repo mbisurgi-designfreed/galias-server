7400fa244ecaee33cb15a8d6c913167f9c707a6a
mbisurgi <maximiliano@designfreed.com>
2018-02-23 15:40:53 -0300

    Eliminar socket io

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const parser = require('body-parser');
-const http = require('http');
-const socket = require('socket.io');
 const cors = require('cors');
 const config = require('./config/config');
 
@@ -17,13 +15,6 @@ app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: false }));
 
-const server = http.createServer(app);
-const io = socket(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected with id: ', socket.client.id);
-});
-
 require('./routes/auth.route')(app);
 require('./routes/canal.route')(app);
 require('./routes/subcanal.route')(app);
@@ -33,8 +24,8 @@ require('./routes/info.route')(app, io);
 require('./routes/pedido.route')(app);
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server started, listening on port ${PORT}`);
 });
 
-module.exports = server;
+module.exports = app;
