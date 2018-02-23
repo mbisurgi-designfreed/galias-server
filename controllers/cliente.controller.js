01ec25f1f0f44bff8c61c37bd5e45ffdf8da2c00
mbisurgi <maximiliano@designfreed.com>
2018-02-23 14:57:06 -0300

    Sincronizar clientes nuevos y actualizados

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
+const config = require('../config/config');
 const Cliente = require('../models/cliente.model');
 
 exports.getById = async (req, res, next) => {
@@ -45,13 +46,13 @@ exports.getClientes = async (req, res, next) => {
 
 exports.insert = async (req, res, next) => {
     try {
-        const cliente = await new Cliente(req.body).save();
+        let cliente = await new Cliente(req.body).save();
 
-        // const sync = await axios.post('localhost:8080/cliente/new', cliente);
+        const sync = await axios.post(`${config.spring.url}/cliente/new`, cliente);
 
-        // if (sync.status === 200) {
-        //     cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
-        // }
+        if (sync.status === 200) {
+            cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
+        }
 
         res.status(201).send(cliente);
     } catch (err) {
@@ -64,13 +65,15 @@ exports.update = async (req, res, next) => {
     try {
         const id = req.params.id;
 
-        const cliente = await Cliente.findByIdAndUpdate(id, { ...req.body, sincronizado: false }, { new: true });
+        let cliente = await Cliente.findByIdAndUpdate(id, { ...req.body, sincronizado: false }, { new: true });
 
-        // const sync = await axios.post(`localhost:8080/cliente/${cliente.codigo}`, cliente);
+        const sync = await axios.patch(`${config.spring.url}/cliente/update`, cliente);
 
-        // if (sync.status === 200) {
-        //     cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
-        // }
+        if (sync.status === 204) {
+            cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
+        } else {
+            cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: false }, { new: true });
+        }
 
         res.status(201).send(cliente);
     } catch (err) {
