abf4193cfbbadb4181d74e01a83df755a464fbba
mbisurgi <maximiliano@designfreed.com>
2018-02-23 15:35:10 -0300

    Agregar variables de entorno pusher y spring

--- config/enviroment/prod.js
+++ config/enviroment/prod.js
@@ -8,5 +8,15 @@ module.exports = {
     twilio: {
         accountSid: process.env.TWILIO_ACCOUNT_SID,
         authToken: process.env.TWILIO_AUTH_TOKEN
+    },
+    pusher: {
+        appId: process.env.PUSHER_APP_ID,
+        key: process.env.PUSHER_KEY,
+        secret: process.env.PUSHER_SECRET,
+        cluster: process.env.PUSHER_CLUSTER,
+        encrypted: true
+    },
+    spring: {
+        url: process.env.SPRING_URL
     }
 }
