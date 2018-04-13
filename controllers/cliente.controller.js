9bc3e1dd57cbb411417cde883249758b4c758b6e
mbisurgi <maximiliano@designfreed.com>
2018-04-13 20:59:56 -0300

    Sacar sync clientes

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -63,11 +63,11 @@ exports.insert = async (req, res, next) => {
     try {
         let cliente = await new Cliente(req.body).save();
 
-        const sync = await axios.post(`${config.spring.url}/cliente/new`, cliente);
+        // const sync = await axios.post(`${config.spring.url}/cliente/new`, cliente);
 
-        if (sync.status === 200) {
-            cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
-        }
+        // if (sync.status === 200) {
+        //     cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
+        // }
 
         res.status(201).send(cliente);
     } catch (err) {
@@ -96,13 +96,13 @@ exports.update = async (req, res, next) => {
 
         let cliente = await Cliente.findByIdAndUpdate(id, { ...req.body, sincronizado: false }, { new: true });
 
-        const sync = await axios.patch(`${config.spring.url}/cliente/update`, cliente);
+        // const sync = await axios.patch(`${config.spring.url}/cliente/update`, cliente);
 
-        if (sync.status === 204) {
-            cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
-        } else {
-            cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: false }, { new: true });
-        }
+        // if (sync.status === 204) {
+        //     cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: true }, { new: true });
+        // } else {
+        //     cliente = await Cliente.findByIdAndUpdate(cliente._id, { sincronizado: false }, { new: true });
+        // }
 
         res.status(201).send(cliente);
     } catch (err) {
